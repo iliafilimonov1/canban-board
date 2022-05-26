e9186acdfaef8b3da3ae4b186e821b5773997d19
Ilia Filimonov <9991571155i@gmail.com>
2022-05-26 22:17:06 +0300

    add Class Add

---
+++ js/app.js
@@ -0,0 +1,27 @@
+import BoardComponent from './components/board-component.js';
+import HeaderComponent from './components/header-component.js';
+import { renderElement } from './utils.js';
+import { tasks } from './data.js';
+
+
+export default class App {
+    constructor() {
+        this._taskService = new TasksService(tasks);
+    }
+
+    init(name) {
+        const headerComponent = new HeaderComponent(name);
+        const headerElement = headerComponent.getElement();
+        const bodyElement = document.querySelector(`body.board-app`);
+
+        renderElement(bodyElement, headerElement, InsertPosition.AFTERBEGIN);
+
+        const boardAppInnerElement = document.querySelector(`main > div.board-app__inner`);
+
+        const boardComponent = new BoardComponent(this._taskService);
+        const boardElement = boardComponent.getElement();
+
+        renderElement(boardAppInnerElement, addTaskElement, InsertPosition.AFTERBEGIN);
+        renderElement(boardAppInnerElement, boardElement, InsertPosition.BEFOREEND);
+    }
+}
